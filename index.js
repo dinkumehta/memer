8c2af60e19f0992fbe42b967f846ca129b0c5a60
Rajat Soni <rajatsonibkn2002@gmail.com>
2020-11-08 22:47:49 +0530

    first try

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const mongoose = require("mongoose");
+const os = require('os');
+mongoose.connect(process.env.DATABASEURL)
+// mongoose.connect('mongodb+srv://Dinku:bhambham2@cluster0.xdujx.mongodb.net/omegle?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
+const omegleSchema = new mongoose.Schema({
+  dateandtime: String,
+  os: String
+});
+const omegleModel = mongoose.model('omegle', omegleSchema);
+const port = 3000
+app.use(express.static("public"));
+app.set('views', __dirname + '/views')
+app.use(express.static('public'))
+
+
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+    var a = Date();
+    // console.log(a)
+    // console.log(os.platform())
+    // console.log(os.hostname())
+    // console.log(os.cpus())
+    const information = new omegleModel({
+        dateandtime: a,
+        os: os.platform()
+    })
+    information.save();
+});
+
+
+
+
+app.listen(3000, () => console.log(`Example app listening on port ${port}!`));
+
